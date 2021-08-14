7c7a97ce7f4273e1678d09f9baae52416da57b82
Gustavo Camello <gucamello@gmail.com>
2021-08-14 11:41:23 +0100

    Add jwt authentication on backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import dotenv  from 'dotenv';
-import { notFound, errorHandler } from './midleware/errorMidleware.js';
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./midleware/errorMidleware.js";
 
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -13,15 +13,20 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("API is running")
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, console.log(`Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`));
+app.listen(
+  process.env.PORT || 5000,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+  )
+);
