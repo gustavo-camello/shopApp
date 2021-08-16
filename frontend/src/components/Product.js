48cebc267e78b10aa5f79736afe2050f67749404
Gustavo Camello <gucamello@gmail.com>
2021-08-16 08:41:12 +0100

    Create user reducers

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Rating from './Rating';
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
     <>
       <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
         <Link to={`/product/${product._id}`}>
-          <img className="hover:grow hover:shadow-lg" src={product.image} alt={product.name}/>
+          <img
+            className="hover:grow hover:shadow-lg"
+            src={product.image}
+            alt={product.name}
+          />
           <div className="pt-3 flex flex-col items-start justify-between">
             <p className="text-sm">{product.name}</p>
             <Rating value={product.numReviews} />
-            <p class="pt-1 text-gray-900 font-bold">€{product.price}</p>
+            <p className="pt-1 text-gray-900 font-bold">€{product.price}</p>
           </div>
-        </Link>        
-      </div> 
+        </Link>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
